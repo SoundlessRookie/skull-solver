feefe72d9cef4c07ebc16990c141741b8c31312e
Mark <57200829+SoundlessRookie@users.noreply.github.com>
2025-12-02 15:37:49 -0800

    app.py: Implement button grid

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import sys
 import globals
+from skull_finder import SkullFinder
 
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QPixmap, QPainter
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QToolButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QToolButton, QWidget, QGridLayout
 
 
 class CellButton(QToolButton):
 
-    def __init__(self, row: int, col: int):
-        super().__init__()
+    def __init__(self, row: int, col: int, skull_finder: SkullFinder = None, window: QWidget = None, parent=None):
+        super().__init__(parent)
         self.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
         self.row = row
         self.col = col
+        self.skull_finder = skull_finder
+        self.window = window
         self.setFixedSize(QSize(64, 64))
         self.setIconSize(QSize(64, 64))
         self.setDisabled(True)
-        self.setCheckable(True)
+        self.setCheckable(False)
         self.update_icon(globals.CELL_UNEXPLORED)
         self.clicked.connect(self.on_click)
 
@@ -56,6 +59,8 @@ class CellButton(QToolButton):
 
     def on_click(self):
         print(f"Button Clicked: {self.get_coordinates()}")
+        self.skull_finder.explore_cell(self.row, self.col)
+        self.window.update_grid()
 
 
 class MainWindow(QMainWindow):
@@ -63,12 +68,33 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Skull Solver")
-        self.setFixedSize(QSize(500, 500))
+        # self.setFixedSize(QSize(500, 500))
 
-        # TODO: Create grid
-        button = CellButton(0, 0)
-        button.setDisabled(False)
-        self.setCentralWidget(button)
+        central = QWidget()
+        layout = QGridLayout(central)
+        layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setCentralWidget(central)
+
+        self.skull_finder = SkullFinder(row_size=7, col_size=7)
+        self.skull_finder.fill_grid()
+
+        self.grid = []
+        for row in range(0, self.skull_finder.row_size):
+            button_row = []
+            for col in range(0, self.skull_finder.col_size):
+                button = CellButton(row=row, col=col, skull_finder=self.skull_finder, window=self)
+                button.setDisabled(False)
+                layout.addWidget(button, row, col)
+                button_row.append(button)
+
+            self.grid.append(button_row)
+
+    def update_grid(self):
+        for row in range(0, self.skull_finder.row_size):
+            for col in range(0, self.skull_finder.col_size):
+                self.grid[row][col].update_icon(self.skull_finder.grid_displayed_data[row][col])
 
 
 app = QApplication(sys.argv)
